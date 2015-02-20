cb167bb3098baa6661dfc538682092af72341909
jvail1 <jvail@upenn.edu>
2015-02-19 23:04:34 -0500

    Programming Assignment 2
    Take shell function and implement a matrix and inverse matrix cache
    test function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Define a matrix and calculate inverse
+##Then store inverse matrix in cache
 
-## Write a short comment describing this function
+## Define matrix and inverse and store both in cache for cache retrieval test
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #set this variable to null
+  m <- NULL
+  #this function creates matrix
+  set <- function(y) {
+    #store variable in cache
+    x <<- y 
+    #store this variable in cache as null
+    m <<- NULL 
+  }
+  #this function finds matrix and calculates inverse
+  get <- function() x 
+  #inverse of original matrix
+  #store inverse in cache
+  setinverse <- function(solve) m <<- solve 
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## Get inverse of Matrix defined in cacheMatrix
+##Look in Cache, if null, then calculate and store in cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ##defined above and stored in cache
+  
+    #get inverse of 'x' from cache and check if null
+    m <- x$getinverse()
+    #if null then calculate using cached value for 'x'
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
